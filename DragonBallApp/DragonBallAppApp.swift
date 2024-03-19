f123ee871037f466d8fd94dd296d915fc6f60af9
Marco Alonso <marcoalonsorota@hotmail.com>
2024-03-19 11:15:22 -0600

    Getting Started

---
+++ DragonBallApp/DragonBallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallAppApp.swift
+//  DragonBallApp
+//
+//  Created by Marco Alonso on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
